96a5295a09946f4b2ffa89a2089b915204241b48
fefe <felix.parendeau@gmail.com>
2025-11-25 10:32:53 +0100

    ex4

---
+++ jour_4/requetes/exercice_3/index.php
@@ -0,0 +1,21 @@
+<?php 
+$host = "localhost";
+$port = "3306";
+$dbname = "sqlintro";
+$connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
+
+$user = "root";
+$password = "";
+
+$db = new PDO(
+    $connexionString,
+    $user,
+    $password
+);
+
+$query = $db->prepare("INSERT INTO users (id, username, email, job) VALUES (NULL, 'Batman', 'bruce@wayne.com', 3)");
+$query->execute();
+$user = $query->fetchall(PDO::FETCH_ASSOC);
+var_dump($user);
+
+?>
